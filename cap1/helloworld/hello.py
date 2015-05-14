cac0011820cad0d59282e6beb8ec61247a4e2a54
Leandro Barbosa <leandro@tree.sh>
2015-05-14 17:32:58 -0300

    update hello with wsgi and gunicorn

--- cap1/helloworld/hello.py
+++ cap1/helloworld/hello.py
@@ -1,10 +1,10 @@
 #coding: utf-8
 import sys
 
-from django.http import HttpResponse
 from django.conf.urls import url
 from django.conf import settings
-
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
 
 settings.configure(
     DEBUG=True,
@@ -25,6 +25,8 @@ def index(request):
 urlpatterns = [
     url(r'^$', index),
 ]
+
+application = get_wsgi_application()
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
