72f20fe1ec5fc0fdeb94f15934e7b0fe67625e0a
Leandro Barbosa <leandro@tree.sh>
2015-05-15 17:44:50 -0300

    creating and writing in image

--- cap2/placeholder/placeholder.py
+++ cap2/placeholder/placeholder.py
@@ -9,7 +9,7 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from django import forms
 
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageDraw
 
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
@@ -38,16 +38,26 @@ class ImageForm(forms.Form):
     width = forms.IntegerField(min_value=1, max_value=2000)
 
 
-def generate(self, image_format='PNG'):
-    height = self.cleaned_data['height']
-    widht = self.cleaned_data['width']
-    image = Image.new('RGB', (width, height))
-    content = BytesIO()
-    return content
+    def generate(self, image_format='PNG'):
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        text = '{} x {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if textwidth < width and textheight < height:
+            texttop = (height - textheight) // 2
+            textleft = (width - textwidth) // 2
+            draw.text((textleft, texttop), text, fill=(
+                255, 255, 255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
 
 
 def placeholder(request, width, height):
-    form = ImageForm({'height':height, 'widht':width})
+    form = ImageForm({'height':height, 'width':width})
     if form.is_valid():
         image = form.generate()
         return HttpResponse(image, content_type='image/png')
